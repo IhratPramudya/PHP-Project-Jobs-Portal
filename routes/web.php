be681c20fffa2c7e02c3b232ccd9638bc79edfa5
ihrat pramudya <ihratpramudya>
2025-01-18 12:07:25 +0700

    add Route version 1 use api

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/jobs', function () {
+    return '<h1>Available Jobs</h1>';
+})->name('jobs');
+
+Route::any('/submit', function () {
+    return 'Submitted';
+});
+
+Route::get('/test', function () {
+    $url = route('jobs');
+    return "<a href=''>Click Here</a>";
+});
+
+Route::get('/api/users', function () {
+    return [
+        'name' => 'John Doe',
+        'email' => 'john@gmail.com'
+    ];
+});
