3b71147a89f18edaac9ba52188883f940a3d265a
ihrat pramudya <ihratpramudya>
2025-01-19 16:36:59 +0700

    Add create jobs page and List Jobs

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
-use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -9,19 +7,9 @@ Route::get('/', function () {
 });
 
 Route::get('/jobs', function () {
-    return '<h1>Available Jobs</h1>';
+    return view('jobs.index');
 })->name('jobs');
 
-Route::get('/test', function () {
-    return response()->json(['name' => 'John Doe'])->cookie('name', 'John Doe');
-});
-
-
-Route::get('/download', function () {
-    return response()->download(public_path('favicon.ico'));
-});
-
-Route::get('/read-cookie', function (Request $request) {
-    $cookieValue = $request->cookie('name');
-    return response()->json(['cookie' => $cookieValue]);
-});
+Route::get('/jobs/create', function () {
+    return view('jobs.create');
+})->name('jobs.create');
